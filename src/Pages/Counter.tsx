f71f8de0ae35327c1b741dd7ace0e3ab777956de
Brielle28 <chukwuemerieclare@gmail.com>
2024-08-13 09:20:34 +0100

    responsiveness

--- src/Pages/Counter.tsx
+++ src/Pages/Counter.tsx
@@ -7,26 +7,31 @@ import { increment, decrement } from "../state/Counter/CounterSlice";
 import { FaHeart } from "react-icons/fa6";
 
 const Counter: React.FC = () => {
-  // Accessing the state from the Redux store
   const count = useSelector((state: RootState) => state.counter.value);
   const dispatch = useDispatch();
 
   return (
-    <div className="flex flex-row items-center justify-center w-full h-screen bg-slate-300 gap-2">
+    <div className="flex flex-row items-center justify-center min-h-screen bg-slate-300 sm:gap-2 md:gap-4 lg:gap-6 xl:gap-10">
       <div className="flex items-center justify-between gap-2">
         {count > 0.00 ? (
-          <FaHeart className="text-[150px] text-red-500" />
+          <FaHeart className="text-[60px] sm:text-[80px] md:text-[100px] lg:text-[120px] xl:text-[150px] text-red-500" />
         ) : (
-          <IoMdHeartEmpty className="text-[150px] text-gray-400" />
+          <IoMdHeartEmpty className="text-[60px] sm:text-[80px] md:text-[100px] lg:text-[120px] xl:text-[150px] text-gray-400" />
         )}
-        <h1 className="text-[150px]">{count.toFixed(2)}</h1>
+        <h1 className="text-[60px] sm:text-[80px] md:text-[100px] lg:text-[120px] xl:text-[150px]">{count.toFixed(2)}</h1>
       </div>
-      <div className="flex flex-col items-center justify-center gap-10">
-        <button onClick={() => dispatch(increment())}>
-          <FaPlus />
+      <div className="flex flex-col items-center justify-center gap-2 md:gap-8 lg:gap-2 xl:gap-12">
+        <button
+          onClick={() => dispatch(increment())}
+          className="p-[2px] text-white rounded-full hover:bg-blue-600 transition-all duration-300"
+        >
+          <FaPlus className="text-[10px] sm:text-[10px] md:text-[10px] lg:text-[20px] xl:text-[20px]" />
         </button>
-        <button onClick={() => dispatch(decrement())}>
-          <FaMinus />
+        <button
+          onClick={() => dispatch(decrement())}
+          className="p-[2px] text-white rounded-full hover:bg-red-600 transition-all duration-300"
+        >
+          <FaMinus className="text-[10px] sm:text-[10px] md:text-[10px] lg:text-[20px] xl:text-[20px]" />
         </button>
       </div>
     </div>
